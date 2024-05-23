af3b62898e01b3cb9fefb0a69ddc8e9dda17894b
Ertugrulatas <ertugrulatas@gmail.com>
2024-05-23 12:12:35 +0300

    categoriler düzenlendi

--- yemek_tarifleri/lib/categories.dart
+++ yemek_tarifleri/lib/categories.dart
@@ -5,7 +5,8 @@ import 'meals.dart';
 
 class CategoriesScreen extends StatelessWidget {
   Future<List<dynamic>> fetchCategories() async {
-    final response = await http.get(Uri.parse('https://www.themealdb.com/api/json/v1/1/categories.php'));
+    final response = await http.get(
+        Uri.parse('https://www.themealdb.com/api/json/v1/1/categories.php'));
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
@@ -21,7 +22,89 @@ class CategoriesScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Yemek Kategorileri'),
       ),
-    body: FutureBuilder<List<dynamic>>(
+      body: FutureBuilder<List<dynamic>>(
+        future: fetchCategories(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Hata: ${snapshot.error}'));
+          } else if (!snapshot.hasData) {
+            return Center(child: Text('Veri Yok'));
+          } else {
+            final categories = snapshot.data!;
+            return GridView.builder(
+              padding: EdgeInsets.all(10.0),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10.0,
+                mainAxisSpacing: 10.0,
+              ),
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                final category = categories[index];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            MealsScreen(category: category['strCategory']),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    elevation: 5,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(10.0)),
+                            child: Image.network(
+                              category['strCategoryThumb'],
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text(
+                            category['strCategory'],
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+
+
+/*
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Yemek Kategorileri'),
+      ),
+      body: FutureBuilder<List<dynamic>>(
         future: fetchCategories(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -42,8 +125,8 @@ class CategoriesScreen extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => MealsScreen(category: category['strCategory']),
-                      
+                        builder: (context) =>
+                            MealsScreen(category: category['strCategory']),
                       ),
                     );
                   },
@@ -56,3 +139,4 @@ class CategoriesScreen extends StatelessWidget {
     );
   }
 }
+*/
